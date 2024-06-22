5e6c7921909a8fffc9c398e91e16ea1691da3fbe
Dhruv Rana <dhruvrana4@gmail.com>
2024-06-22 11:55:27 +0530

    feat: add is_completed to check the task status

--- todo_api/models.py
+++ todo_api/models.py
@@ -5,3 +5,4 @@ from django.db import models
 class TODO(models.Model):
     task=models.CharField(max_length=100)
     created_at =models.DateTimeField(auto_now_add=True)
+    is_completed= models.BooleanField(default=False)
--- todo_api/serializer.py
+++ todo_api/serializer.py
@@ -6,5 +6,5 @@ class TodoSerializer(serializers.ModelSerializer):
     task = serializers.CharField(max_length=100, required=True)
     class Meta:
         model = TODO
-        fields = ['id', 'task', 'created_at']
+        fields = ['id', 'task', 'created_at','is_completed']
         
